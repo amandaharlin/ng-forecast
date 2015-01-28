710b1b1d758ca30aeace7a0dfa2b963e2ea8cd5d
Amanda Harlin <amanda.n.harlin@gmail.com>
2015-01-27 19:21:35 -0600

    learning about ui router resolve

--- app/scripts/views/forecast.js
+++ app/scripts/views/forecast.js
@@ -3,21 +3,22 @@
 ForecastApp
   .config(function dailyWeather($stateProvider) {
 
-    function forecastResolve(location, $stateParams) {
-      return location.get($stateParams.location);
+    function forecastResolve(forecast) {
+        var listofForecast = Restangular.get('forecasts');
     }
 
     var forecastView = {
       url: '/forecast?location',
       templateUrl: './views/forecast.html',
-      resolve: forecastResolve,
-      controller: 'forecastViewCtrl'
+      controller: 'forecastViewCtrl',
+      resolve: forecastResolve
     };
+
     $stateProvider
       .state('weatherforecaster.forecast', forecastView);
   });
 
 ForecastApp
-  .controller('forecastViewCtrl', function forecastViewCtrl($scope) {
-
+  .controller('forecastViewCtrl', function forecastViewCtrl($scope, forecast) {
+    $scope.forecast = forecast;
   });
