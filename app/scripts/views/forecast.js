66f5ff0480a3c60387d5d36ff42fa47fddbd1c1d
Amanda Harlin <amanda.n.harlin@gmail.com>
2015-01-30 00:33:17 -0600

    fixed forecast typo, ugh, but got data off of forecast_info. sidenot, chrome devtools isnt displaying the underscore, so it threw me off for a little bit

--- app/scripts/views/forecast.js
+++ app/scripts/views/forecast.js
@@ -28,11 +28,12 @@ ForecastApp
 ForecastApp
   .controller('forecastViewCtrl', function forecastViewCtrl($scope, forecast) {
     var wx = forecast.forecast || {};
-    $scope.forecast = wx;
+    $scope.forecast = forecast;
     $scope.forecastInfo = wx['forecast_info'][0];
     $scope.dailySummary = wx.daily_summary[0];
 
-
+    $scope.location = wx.forecast_info[0].location[0].$;
+    $scope.location.city = wx.forecast_info[0].location[0]._;
     $scope.wxIcon = wx.daily_summary[0].wx_icon[0];
 
     console.log(forecast);
